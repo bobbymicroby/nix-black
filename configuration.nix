d264bf5498d7603d19cd539f2083b3f23701b5f1
Bobby Microby <bobymicroby@gmail.com>
2019-01-05 13:41:21 +0000

    enable pulseaudio and 32Bit dri

--- configuration.nix
+++ configuration.nix
@@ -2,37 +2,60 @@
 { config, pkgs, ... }:
 
 {
-  imports =
-    [ 
-      ./hardware-configuration.nix
-    ];
+imports =
+  [ 
+     ./hardware-configuration.nix
+];
 
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.initrd.luks.devices = [
-  {
+boot.loader.systemd-boot.enable = true;
+boot.loader.efi.canTouchEfiVariables = true;
+boot.initrd.luks.devices = [
+ {
   name = "root";
   device = "/dev/disk/by-uuid/37804d89-4c68-42e1-99fd-22deef9b525d";
   preLVM = true;
-  }
-  ];
+ }
+];
    
-  networking.networkmanager.enable = true;
-  users.extraUsers.bobby = {
-  createHome=true;
-  extraGroups  = ["wheel" "video" "audio" "disk" "networkmanager"];
-  group = "users";
-  home  ="/home/bobby";
-  isNormalUser = true;
-  uid = 1000;
-  }; 
+networking.networkmanager.enable = true;
+networking.hostName = "black-nixos";
+
+users.extraUsers.bobby = {
+createHome=true;
+extraGroups  = ["wheel" "video" "audio" "disk" "networkmanager"];
+group = "users";
+home  ="/home/bobby";
+isNormalUser = true;
+uid = 1000;
+}; 
+
+hardware.pulseaudio.enable = true;
+hardware.pulseaudio.support32Bit = true;
+
+hardware.opengl.driSupport32Bit = true;
 
 nixpkgs.config.allowUnfree = true;  
 
+
+
+environment.systemPackages = with pkgs; [
+vim
+gitFull 
+htop
+which
+file
+];
+
+
+services.upower.enable = true;
+
 services.xserver = {
+
 
 enable=true;
 
+autoRepeatDelay = 300;
+autoRepeatInterval = 50;
 
 displayManager = {
 
@@ -60,7 +83,8 @@ screenSection = ''
 windowManager.xmonad = {
 
 enable = true;
-enableContribAndExtras = true;
+
+
 };
 
 
@@ -68,9 +92,6 @@ enableContribAndExtras = true;
 
 
 
-  environment.systemPackages = with pkgs; [
-vim git 
-  ];
 
   system.stateVersion = "18.09"; 
 
